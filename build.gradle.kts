517fbff0176193ad3e2a053693063aa769535ecd
World (Gi-hyeon) <simworld415@gmail.com>
2023-08-20 20:37:52 +0900

    Initial plugin commit

---
+++ build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    java
+    `java-library`
+    kotlin("jvm") version "1.9.0"
+    id("io.papermc.paperweight.userdev") version "1.5.5"
+    id("xyz.jpenilla.run-paper") version "2.1.0" // Adds runServer and runMojangMappedServer tasks for testing
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "dev.worldsw"
+version = "1.0-SNAPSHOT"
+description = "The plugin for the architecture king(YouTuber)."
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+dependencies {
+    paperweight.paperDevBundle("1.20.1-R0.1-SNAPSHOT")
+
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
+
+    testImplementation(kotlin("test"))
+}
+
+val pluginName = rootProject.name.split('-').joinToString("") { it.replaceFirstChar(Char::titlecase) }
+
+tasks {
+    processResources {
+        filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
+        val props = mapOf(
+            "name" to project.name,
+            "version" to project.version,
+            "description" to project.description,
+            "apiVersion" to "1.20"
+        )
+        inputs.properties(props)
+        filesMatching("*plugin*.yml") {
+            expand(props)
+        }
+    }
+
+    test {
+        useJUnitPlatform()
+    }
+
+    runServer {
+        minecraftVersion("1.20.1")
+        jvmArgs("-Dcom.mojang.eula.agree=true")
+    }
+
+    assemble {
+        dependsOn(reobfJar)
+        dependsOn(shadowJar)
+    }
+
+    compileJava {
+        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+
+        // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
+        // See https://openjdk.java.net/jeps/247 for more information.
+        options.release.set(17)
+    }
+
+    javadoc {
+        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+    }
+
+    shadowJar {
+        dependencies {
+            include(dependency("org.jetbrains.kotlin:kotlin-stdlib"))
+        }
+    }
+
+    task<Exec>("uploadToServer") {
+        dependsOn(assemble)
+        commandLine("scp", "./build/libs/${project.name}-${project.version}.jar", "ubuntu@xxx.xxx.xxx.xxx:/home/ubuntu/xxx/plugins")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+rootProject.name = "ArchKing"
